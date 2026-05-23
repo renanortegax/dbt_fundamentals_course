7b31bd834f0af0d46f2762ee45f524de7c6f029f
renanortegax <renan.xavier44@gmail.com>
2026-05-23 20:01:42 -0300

    Recreating stg files using generate_base_model macro from codegen dbt package

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -1,5 +1,18 @@
-select
-    id as customer_id,
-    first_name,
-    last_name
-from {{ source('jaffle_shop', 'customers') }} --name of the source + name of the table
+with source as (
+
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        id as customer_id
+        , first_name
+        , last_name
+
+    from source
+
+)
+
+select * from renamed
--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,9 +1,21 @@
-select
-    id as payment_id
-    ,orderid as order_id
-    ,paymentmethod as payment_method
-    ,status
-    -- cents to dolla
-    ,amount / 100 as amount
-    ,created
-from {{ source('stripe','payments') }}
+with source as (
+
+    select * from {{ source('stripe', 'payments') }}
+
+),
+
+renamed as (
+
+    select
+        id as payment_id
+        , orderid as order_id
+        , paymentmethod as payment_method
+        , status
+        , amount / 100 as amount
+        , created
+
+    from source
+
+)
+
+select * from renamed
